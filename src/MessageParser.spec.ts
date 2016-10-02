57e09b5e897377da7fa8170ee973a2f761680e16
Mattias Kindborg <mattias.kindborg@gmail.com>
2016-10-02 18:07:00 +0200

    Configure sample tests

---
+++ src/MessageParser.spec.ts
@@ -0,0 +1,11 @@
+import {expect} from 'chai';
+
+describe('Sample tests', function() {
+	it('test 1', function() {
+		expect(1).to.equal(1);
+	});
+
+	it('test 2', function() {
+		expect(1).to.equal(1);
+	});
+});
