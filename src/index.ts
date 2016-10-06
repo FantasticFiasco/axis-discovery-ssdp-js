87d5f53f3dae563eb183b88172e64f4b33be831b
Mattias Kindborg <mattias.kindborg@gmail.com>
2016-10-06 23:45:13 +0200

    Print time

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import { Device } from './Device';
 const discovery = new SsdpDiscovery();
 
 discovery.on('hello', (device: Device) => {
-	console.log(`Hello from ${device.serialNumber} on ${device.address}`);
+	console.log(`${new Date()} Hello from ${device.serialNumber} on ${device.address}`);
 });
 
 discovery.on('goodbye', (device: Device) => {
-	console.log(`Goodbye from ${device.serialNumber} on ${device.address}`);
+	console.log(`${new Date()} Goodbye from ${device.serialNumber} on ${device.address}`);
 });
 
 discovery.start();
