74257549d536553246c44e9a22a6f05d399a30d0
Mattias Kindborg <mattias.kindborg@gmail.com>
2016-11-30 23:46:15 +0100

    Concat declarations
    This is really a hack to generate the TypeScript declarations and concat them into one file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var concat = require('gulp-concat');
 var mocha = require('gulp-mocha');
 var tslint = require("gulp-tslint");
 var del = require('del');
@@ -8,14 +9,23 @@ var tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('clean', function() {
 	return del([
-		'lib/**/*'
+		'./lib/**/*'
 	]);
 });
 
-gulp.task('build', ['clean'], function() {
-	return gulp.src('src/**/*.ts')
+gulp.task('compile', ['clean'], function() {
+	return gulp.src('./src/**/*.ts')
 		.pipe(tsProject())
-		.pipe(gulp.dest("lib"));
+		.pipe(gulp.dest("./lib/"));
+});
+
+gulp.task('generate-declaration', ['compile'], function() {
+	return gulp.src(['./lib/Discovery.d.ts', './lib/Device.d.ts'])
+		.pipe(concat('axis.discovery.ssdp.d.ts'))
+		.pipe(gulp.dest('./lib/'));
+});
+
+gulp.task('build', ['generate-declaration'], function() {
 });
 
 gulp.task('test', ['build'], function() {
