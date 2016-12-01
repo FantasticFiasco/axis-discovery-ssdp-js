a563422ee3a24b409d5cfe7eaed2a5bfdf92007b
Mattias Kindborg <mattias.kindborg@gmail.com>
2016-12-01 19:45:34 +0100

    Refactor how declarations are exported

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
-var concat = require('gulp-concat');
 var mocha = require('gulp-mocha');
 var tslint = require("gulp-tslint");
 var del = require('del');
@@ -8,9 +7,7 @@ var del = require('del');
 var tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('clean', function() {
-	return del([
-		'./lib/**/*'
-	]);
+	return del(['./lib/**/*']);
 });
 
 gulp.task('compile', ['clean'], function() {
@@ -19,13 +16,7 @@ gulp.task('compile', ['clean'], function() {
 		.pipe(gulp.dest("./lib/"));
 });
 
-gulp.task('generate-declaration', ['compile'], function() {
-	return gulp.src(['./lib/Discovery.d.ts', './lib/Device.d.ts'])
-		.pipe(concat('axis.discovery.ssdp.d.ts'))
-		.pipe(gulp.dest('./lib/'));
-});
-
-gulp.task('build', ['generate-declaration'], function() {
+gulp.task('build', ['compile'], function() {
 });
 
 gulp.task('test', ['build'], function() {
---
+++ src/barrel.ts
@@ -0,0 +1,4 @@
+// This file will act as the barrel for the package,
+// exporting the public API to consumers.
+export * from './Device';
+export * from './Discovery';
