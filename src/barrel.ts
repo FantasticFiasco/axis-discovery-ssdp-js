b085dea04ab760b88ac8d5bb76de4d17302bad2c
Mattias Kindborg <mattias.kindborg@gmail.com>
2016-12-03 12:24:28 +0100

    Add demo application
    Release 0.0.8

--- src/barrel.ts
+++
@@ -1,4 +0,0 @@
-// This file will act as the barrel for the package,
-// exporting the public API to consumers.
-export * from './Device';
-export * from './Discovery';
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,2 @@
-import { Discovery } from './Discovery';
-import { Device } from './Device';
-
-const discovery = new Discovery();
-
-discovery.onHello((device: Device) => {
-    console.log(`${new Date().toLocaleTimeString()} - Hello from ${device.serialNumber} on ${device.address}:${device.port}`);
-    console.log(`JSON:\r\n${JSON.stringify(device)}`);
-});
-
-discovery.onGoodbye((device: Device) => {
-    console.log(`${new Date().toLocaleTimeString()} - Goodbye from ${device.serialNumber} on ${device.address}`);
-});
-
-discovery.start();
+export * from './Device';
+export * from './Discovery';
