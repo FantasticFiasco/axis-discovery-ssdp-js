4c4ee4e1b8a84908cf368346c73faacf1cfcd2e9
FantasticFiasco <mattias.kindborg@gmail.com>
2017-07-04 19:02:41 +0200

    wip

--- src/logging/Log.ts
+++ src/logging/Log.ts
@@ -1,3 +1,7 @@
-export function log(message?: any) {
-    console.log(message);
+import * as debug from 'debug';
+
+export function log(formatter: any, ...args: any[]) {
+    logger(formatter, args);
 }
+
+const logger = debug('axis-discovery-ssdp');
