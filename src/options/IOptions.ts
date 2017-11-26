b6068c15f58b8aa2caf2411204538c937df63da3
FantasticFiasco <mattias.kindborg@gmail.com>
2017-11-26 19:46:35 +0100

    wip

--- src/options/IOptions.ts
+++ src/options/IOptions.ts
@@ -3,16 +3,13 @@
  */
 export interface IOptions {
     /**
-     * Send GET request and await its response.
-     *
-     * The interface responsible for sending HTTP requests on the network.
-     * Default value is based on
-     * <a href="https://www.npmjs.com/package/request">Request</a> but custom
-     * implementations can be provided. This can be useful if discovery is
-     * required in an Electron application and one wish to benefit from the
-     * proxy configuration provided by using Electron's
+     * Function sending GET requests over the network. Default value is based
+     * on <a href="https://www.npmjs.com/package/request">Request</a> but a
+     * custom implementations can be provided. This can be useful if discovery
+     * is required in an Electron application where one wish to benefit from
+     * the proxy configuration provided by using Electron's
      * <a href="https://electronjs.org/docs/api/net">net.request</a>.
-     * @param url: Fully qualified URI.
+     * @param url: Fully qualified URL.
      * @returns Promise with response body.
      */
     getRequest?(url: string): Promise<string>;
