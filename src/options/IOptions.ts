4b306a501eb26b1ca50d9e6bd0485648c170ca34
FantasticFiasco <mattias.kindborg@gmail.com>
2017-11-27 17:55:33 +0100

    wip

--- src/options/IOptions.ts
+++ src/options/IOptions.ts
@@ -7,7 +7,7 @@ export interface IOptions {
     /**
      * An implementation of a HTTP client. Default value is based
      * on <a href="https://www.npmjs.com/package/request">Request</a> but a
-     * custom implementations can be provided. This can be useful if discovery
+     * custom implementation can be provided. This can be useful if discovery
      * is required in an Electron application where one wish to benefit from
      * the proxy configuration provided by using Electron's
      * <a href="https://electronjs.org/docs/api/net">net.request</a>.
