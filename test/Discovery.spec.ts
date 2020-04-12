3933b7de671945b7cbbb41429214e75405816a5d
FantasticFiasco <mattias.kindborg@gmail.com>
2020-04-12 22:16:44 +0200

    wip

--- test/Discovery.spec.ts
+++ test/Discovery.spec.ts
@@ -1,4 +1,3 @@
-import * as dgram from 'dgram';
 import * as os from 'os';
 import { mocked } from 'ts-jest/utils';
 
@@ -6,28 +5,19 @@ import { Discovery } from './../src/Discovery';
 import { NETWORK_INTERFACES_WITH_TWO_ADDRESSES } from './network-interfaces/NetworkInterface.mock';
 
 jest.mock('os');
+jest.mock('dgram');
 
 describe('Discovery', () => {
 
     const osMock = mocked(os);
-    let dgramStub: sinon.SinonStub;
-    let socket: any;
     let discovery: Discovery;
 
     beforeEach(() => {
         // Mock os
         osMock.networkInterfaces.mockReturnValue(NETWORK_INTERFACES_WITH_TWO_ADDRESSES);
 
-        // Mock dgram
-        socket = {
-            bind: function () { },
-            close: function () { },
-            on: function () { },
-            send: function () { },
-        };
-
-        dgramStub = sinon.stub(dgram, 'createSocket')
-            .returns(socket);
+        // Reset manual mocks
+        jest.requireMock('dgram').mockReset();
 
         // Create discovery
         discovery = new Discovery();
@@ -35,53 +25,41 @@ describe('Discovery', () => {
 
     describe('#start', () => {
         test('should not send M-SEARCH messages', async () => {
-            // Arrange
-            const socketBind = sinon.stub(socket, 'bind')
-                .callsArg(2);   // callback is the third argument
-
             // Act
             await discovery.start();
 
             // Assert
-            expect(socketBind.callCount).toBe(3);   // 1 passive and 2 active
+            const socketBindCallCount = jest.requireMock('dgram').socketBindCallCount;
+            expect(socketBindCallCount).toBe(3);   // 1 passive and 2 active
         });
     });
 
     describe('#search', () => {
         test('should send M-SEARCH messages', async () => {
             // Arrange
-            sinon.stub(socket, 'bind')
-                .callsArg(2);   // callback is the third argument
-
-            const socketSend = sinon.stub(socket, 'send')
-                .callsArg(5);   // callback is the sixth argument
-
             await discovery.start();
 
             // Act
             await discovery.search();
 
             // Assert
-            expect(socketSend.callCount).toBe(2);
+            // expect(socketSend.callCount).toBe(2);
+            const socketSendCallCount = jest.requireMock('dgram').socketSendCallCount;
+            expect(socketSendCallCount).toBe(2);
         });
     });
 
     describe('#stop', () => {
         test('should close sockets', async () => {
             // Arrange
-            sinon.stub(socket, 'bind')
-                .callsArg(2);   // callback is the third argument
-
-            const socketClose = sinon.stub(socket, 'close')
-                .callsArg(0);   // callback is the first argument
-
             await discovery.start();
 
             // Act
             await discovery.stop();
 
             // Assert
-            expect(socketClose.callCount).toBe(3);   // 1 passive and 2 active
+            const socketCloseCallCount = jest.requireMock('dgram').socketCloseCallCount;
+            expect(socketCloseCallCount).toBe(3);   // 1 passive and 2 active
         });
     });
 });
